08bf3369f1a439cceb86aca5f754873b1a2d64f8
rohit1239-tech <yengantiwarrohit1239@gmail.com>
2026-01-18 17:37:55 +0530

    Added multiple functions examples

---
+++ Python Fundamentals/multiple_function.py
@@ -0,0 +1,28 @@
+arr = [5,6,8,7]
+
+#to print minimum element 
+print(min(arr))
+
+#to print max element 
+print(max(arr))
+
+#same for string , for string just we have to add key 
+
+
+#sum 
+print(sum(arr, start=10))
+
+#for product we have to improt math 
+import math
+
+print(math.prod(arr))
+
+#any and all which we used in boolean
+arr=[True , False , True ]
+
+print(any(arr))
+print(all(arr))
+
+#count element 
+arr =[1,5,1,7]
+print(arr.count(1))
